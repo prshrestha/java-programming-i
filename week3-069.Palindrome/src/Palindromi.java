187d49cb1058ac71e5f75c5b36cf2940e148ec52
cpflyingduck <prashanta@Prashantas-MBP.home>
2015-08-06 18:33:04 -0400

    exercise 69

--- week3-069.Palindrome/src/Palindromi.java
+++ week3-069.Palindrome/src/Palindromi.java
@@ -1,17 +1,29 @@
+
 import java.util.Scanner;
 
 public class Palindromi {
 
     public static boolean palindrome(String text) {
-        // write code here
-        return false;
+        String reverse = "";
+        int length = text.length();
+        int i = 1;
+        while (i <= length) {
+            reverse += text.charAt(length - i);
+            i++;
+        }
+
+        if (text.equals(reverse)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+
         System.out.println("Type a text: ");
-        String text = reader.nextLine();    
+        String text = reader.nextLine();
         if (palindrome(text)) {
             System.out.println("The text is a palindrome!");
         } else {
